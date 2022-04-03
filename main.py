a93f76f9be788a6463b0e75dc327ec7a05ac2b97
liuxinq <wxsx0526@126.com>
2022-04-03 14:48:53 +0800

    implementing by KNNWithMeans

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from utils import map_genre
 import json
 from surprise import dump
 from surprise import KNNBasic
+from surprise import KNNWithMeans
 from surprise import Dataset
 
 app = FastAPI()
@@ -26,7 +27,7 @@ app.add_middleware(
 )
 
 # =======================DATA=========================
-data = pd.read_csv("movie_info.csv")
+data = pd.read_csv("movies_update.csv")
 
 """
 =================== Body =============================
@@ -96,11 +97,11 @@ async def add_recommend(item_id):
 
 
 def user_add(iid, score):
-    user = '944'
+    user = '611'
     # simulate adding a new user into the original data file
-    df = pd.read_csv('./u.data')
-    df.to_csv('new_' + 'u.data')
-    with open(r'new_u.data',mode='a',newline='',encoding='utf8') as cfa:
+    df = pd.read_csv('./ratings.csv')
+    df.to_csv('new_' + 'ratings.csv')
+    with open(r'new_ratings.csv',mode='a',newline='',encoding='utf8') as cfa:
         wf = csv.writer(cfa,delimiter='\t')
         data_input = []
         s = [user,str(iid),int(score),'0']
@@ -111,16 +112,40 @@ def user_add(iid, score):
 def get_initial_items(iid, score, n=12):
     res = []
     user_add(iid, score)
-    file_path = os.path.expanduser('new_u.data')
+    file_path = os.path.expanduser('new_ratings.csv')
     reader = Reader(line_format='user item rating timestamp', sep='\t')
     data = Dataset.load_from_file(file_path, reader=reader)
     trainset = data.build_full_trainset()
-    algo = KNNBasic(sim_options={'name': 'pearson', 'user_based': False})
+    # algo = KNNBasic(sim_options={'name': 'pearson', 'user_based': False})
+    algo = KNNWithMeans(sim_options={'name': 'pearson', 'user_based': False})
     algo.fit(trainset)
     dump.dump('./model',algo=algo,verbose=1)
     all_results = {}
-    for i in range(1682):
-        uid = str(944)
+    for i in range(10000):
+        uid = str(611)
+        iid = str(i)
+        pred = algo.predict(uid,iid).est
+        all_results[iid] = pred
+    sorted_list = sorted(all_results.items(), key = lambda kv:(kv[1], kv[0]), reverse=True)
+    for i in range(n):
+        print(sorted_list[i])
+        res.append(sorted_list[i][0])
+    return res
+
+def get_initial_items(iid, score, n=12):
+    res = []
+    user_add(iid, score)
+    file_path = os.path.expanduser('new_ratings.csv')
+    reader = Reader(line_format='user item rating timestamp', sep='\t')
+    data = Dataset.load_from_file(file_path, reader=reader)
+    trainset = data.build_full_trainset()
+    # algo = KNNBasic(sim_options={'name': 'pearson', 'user_based': False})
+    algo = KNNWithMeans(sim_options={'name': 'pearson', 'user_based': False})
+    algo.fit(trainset)
+    dump.dump('./model',algo=algo,verbose=1)
+    all_results = {}
+    for i in range(10000):
+        uid = str(611)
         iid = str(i)
         pred = algo.predict(uid,iid).est
         all_results[iid] = pred
@@ -132,11 +157,11 @@ def get_initial_items(iid, score, n=12):
 
 def get_similar_items(iid, n=12):
     algo = dump.load('./model')[1]
+    print("iid", iid)
     inner_id = algo.trainset.to_inner_iid(iid)
-    print(inner_id)
     neighbors = algo.get_neighbors(inner_id, k=n)
     neighbors_iid = [algo.trainset.to_raw_iid(x) for x in neighbors]
-    print(neighbors_iid)
+    print("neighbors_iid", neighbors_iid)
     return neighbors_iid
 
 if __name__ == '__main__':
