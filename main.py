2ab74d59972505bca97fe55360b77af1a686bc56
liuxinq <wxsx0526@126.com>
2022-04-06 17:06:06 +0800

    add average rating

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ from surprise import KNNWithMeans
 from surprise import Dataset
 import random
 from surprise import accuracy
+from recommender import get_recommend_items_by_svd, get_similar_items_by_svd
+
 
 app = FastAPI()
 app.add_middleware(
@@ -61,8 +63,8 @@ def get_genre():
                       "Documentary", "Drama", "Fantasy", "Film_Noir", "Horror", "Musical", "Mystery",
                       "Romance", "Sci_Fi", "Thriller", "War", "Western"]}
 '''
-
-@app.post("/api/movies")
+# == == == == == == == == == KNN with Means == == == == == == == == == == =
+@app.post("/api/movies_knn")
 def get_movies(genre: list):
     print(genre)
     query_str = " or ".join(map(map_genre, genre))
@@ -72,100 +74,103 @@ def get_movies(genre: list):
     return json.loads(results.to_json(orient="records"))
 
 
-@app.post("/api/recommend")
+@app.post("/api/recommend_knn")
 def get_recommend(movies: List[Movie]):
     # print(movies)
     iid = str(sorted(movies, key=lambda i: i.score, reverse=True)[0].movie_id)
     score = int(sorted(movies, key=lambda i: i.score, reverse=True)[0].score)
-    res = get_initial_items(iid,score)
+    new_user_ratings = [(str(i.movie_id), int(i.score)) for i in movies]
+    uid, res = get_initial_items(new_user_ratings)
     res_id = []
     for i in res:
         res_id.append(int(i[0]))
+    # res = get_initial_items(iid,score)
     if len(res_id) > 12:
         res_id = res_id[:12]
-    print(res_id)
     rec_movies = data.loc[data['movie_id'].isin(res_id)]
-    print(rec_movies)
-
-    rec_movies.loc[:, 'like'] = None
-    rec_movies.loc[:, 'ground_truth'] = 0
-    rec_movies.loc[:, 'pred'] = 0
-    print(rec_movies.iloc[0,:])
+    rec_temp = pd.DataFrame(rec_movies)
+    rec_temp.loc[:, 'like'] = None
+    rec_temp.loc[:, 'ground_truth'] = 0
+    rec_temp.loc[:, 'pred'] = 0
     for i in range(12):
         rec_movies.iloc[i, 27] = res[i][1]
-    results = rec_movies.loc[:, ['movie_id', 'movie_title', 'release_date', 'poster_url', 'like', 'ground_truth', 'pred']]
-    print(results)
-    return json.loads(results.to_json(orient="records"))
+    movies = rec_movies.loc[:, [
+        'movie_id', 'movie_title', 'release_date', 'poster_url', 'like', 'ground_truth', 'pred']]
+    results = {'user_id': uid, 'movies': movies.to_json(orient="records")}
+    return json.loads(json.dumps(results))
 
 
-@app.get("/api/add_recommend/{item_id}")
+@app.get("/api/add_recommend_knn/{item_id}")
 async def add_recommend(item_id):
-    res = get_similar_items(str(item_id), n=5)
+    res = get_similar_items_knn(str(item_id), n=5)
     res = [int(i) for i in res]
-    print(res)
-    rec_movies = data.loc[data['movie_id'].isin(res)]
-    print(rec_movies)
-    rec_movies.loc[:, 'like'] = None
-    rec_movies.loc[:, 'ground_truth'] = 0
-    rec_movies.loc[:, 'pred'] = 5
-    results = rec_movies.loc[:, ['movie_id', 'movie_title', 'release_date', 'poster_url', 'like','ground_truth','pred']]
+    similar_movies = data.loc[data['movie_id'].isin(res)]
+    rec_temp = pd.DataFrame(similar_movies)
+    rec_temp.loc[:, 'like'] = None
+    rec_temp.loc[:, 'ground_truth'] = 0
+    rec_temp.loc[:, 'pred'] = 5
+    results = similar_movies.loc[:, ['movie_id', 'movie_title', 'release_date', 'poster_url', 'like','ground_truth','pred']]
     return json.loads(results.to_json(orient="records"))
 
 # evaluation & accuracy
 @app.post("/api/evaluation")
 async def run_eval(movies:list):
     predictions = []
+    ratings = []
     for i in movies:
-        print(i['pred'])
-        predictions.append(['611', i['movie_id'], i['ground_truth'], i['pred'], False])
-    print(predictions)
+        predictions.append([i['user_id'], i['movie_id'], i['ground_truth'], i['pred'], False])
+    for i in range(len(predictions)):
+        print(predictions[i][2])
+        ratings.append(predictions[i][2])
+    avg_rating = np.mean(ratings)
+    print(avg_rating)
     accuracy.rmse(predictions, verbose=True)
 
 
-def user_add(iid, score):
-    user = '611'
+def user_add(new_user_ratings, is_reset=False):
     # simulate adding a new user into the original data file
-    df = pd.read_csv('./ratings.csv')
-    df.to_csv('new_' + 'ratings.csv')
-    with open(r'new_ratings.csv',mode='a',newline='',encoding='utf8') as cfa:
-        wf = csv.writer(cfa,delimiter='\t')
-        data_input = []
-        s = [user,str(iid),int(score),'0']
-        data_input.append(s)
-        for k in data_input:
-            wf.writerow(k)
-
-def get_initial_items(iid, score, n=12):
-    random_res = []
-    user_add(iid, score)
+    if is_reset or not os.path.exists('new_ratings.csv'):
+        df = pd.read_csv('./ratings.csv')
+        df.to_csv('new_' + 'ratings.csv', index=False)
+    new_df = pd.read_csv('./new_ratings.csv', sep='\t', header=None, names=['user', 'item', 'rating', 'timestamp'],index_col=None)
+    uid = int(new_df['user'].max()) + 1
+    with open(r'new_ratings.csv', mode='a', newline='', encoding='utf8') as cfa:
+        wf = csv.writer(cfa, delimiter='\t')
+        for (i, s) in new_user_ratings:
+            if s > 0:
+                new_rating = [str(uid), str(i), int(s), '0']
+                wf.writerow(new_rating)
     file_path = os.path.expanduser('new_ratings.csv')
     reader = Reader(line_format='user item rating timestamp', sep='\t')
     data = Dataset.load_from_file(file_path, reader=reader)
     trainset = data.build_full_trainset()
+    return trainset
+
+def get_initial_items(new_user_ratings, n=12):
+    random_rec = []
+    trainset = user_add(new_user_ratings)
+    uid = trainset.n_users
+    item_rid_list = [trainset.to_raw_iid(inner_id) for inner_id in trainset.ir.keys()]
+    print(f'number of total users: {trainset.n_users}')
+
     # algo = KNNBasic(sim_options={'name': 'pearson', 'user_based': False})
-    algo = KNNWithMeans(sim_options={'name': 'pearson', 'user_based': False})
+    algo = KNNWithMeans(k=60, sim_options={'name': 'pearson', 'user_based': False})
     algo.fit(trainset)
     dump.dump('./model',algo=algo,verbose=1)
     all_results = {}
-    for i in range(193609):
-        uid = str(611)
+    for i in item_rid_list:
+        uid = str(uid)
         iid = str(i)
         pred = algo.predict(uid,iid).est
         if pred >= 5:
             all_results[iid] = pred
     for key in all_results:
-        random_res = random.sample(all_results.items(), 12)
+        random_rec = random.sample(all_results.items(), 12)
+
+    return uid, random_rec
 
 
-    print(random_res)
-    #     all_results[iid] = pred
-    # sorted_list = sorted(all_results.items(), key = lambda kv:(kv[1], kv[0]), reverse=True)
-    # for i in range(n):
-    #     print('sorted_list:',sorted_list[i])
-    #     res.append(sorted_list[i])
-    return random_res
-
-def get_similar_items(iid, n=12):
+def get_similar_items_knn(iid, n=12):
     algo = dump.load('./model')[1]
     print("iid", iid)
     inner_id = algo.trainset.to_inner_iid(iid)
@@ -174,6 +179,55 @@ def get_similar_items(iid, n=12):
     print("neighbors_iid", neighbors_iid)
     return neighbors_iid
 
+# == == == == == == == == == SVD == == == == == == == == == == =
+# @app.post("/api/movies_svd")
+# def get_movies(genre: list):
+#     print(genre)
+#     query_str = " or ".join(map(lambda x: ''+x+'==1', genre))
+#     results = data.query(query_str)
+#     # results.loc[:, 'score'] = None
+#     res_temp = pd.DataFrame(results)
+#     res_temp.loc[:, 'score'] = None
+#     results = results.sample(
+#         18).loc[:, ['movie_id', 'movie_title', 'release_date', 'poster_url', 'score']]
+#     return json.loads(results.to_json(orient="records"))
+
+
+# @app.post("/api/recommend_svd")
+# def get_recommend(movies: List[Movie]):
+#     # print(movies)
+#     # iid = str(sorted(movies, key=lambda i: i.score, reverse=True)[0].movie_id)
+#     # score = int(sorted(movies, key=lambda i: i.score, reverse=True)[0].score)
+#     new_user_ratings = [(str(i.movie_id), int(i.score)) for i in movies]
+#     uid, res = get_recommend_items_by_svd(new_user_ratings)
+#     res = [int(i) for i in res]
+#     if len(res) > 12:
+#         res = res[:12]
+#     print(res)
+#     rec_movies = data.loc[data['movie_id'].isin(res)]
+#     # print(rec_movies)
+#     # rec_movies.loc[:, 'like'] = None
+#     rec_temp = pd.DataFrame(rec_movies)
+#     rec_temp.loc[:, 'like'] = None
+#     movies = rec_movies.loc[:, [
+#         'movie_id', 'movie_title', 'release_date', 'poster_url', 'like']]
+#     results = {'user_id': uid, 'movies': movies.to_json(orient="records")}
+#     return json.loads(json.dumps(results))
+
+
+# @app.get("/api/add_recommend_svd/{item_id}")
+# async def add_recommend(item_id):
+#     res = get_similar_items_by_svd(str(item_id), n=5)
+#     res = [int(i) for i in res]
+#     # print(res)
+#     similar_movies = data.loc[data['movie_id'].isin(res)]
+#     # print(rec_movies)
+#     # rec_movies.loc[:, 'like'] = None
+#     rec_temp = pd.DataFrame(similar_movies)
+#     rec_temp.loc[:, 'like'] = None
+#     results = similar_movies.loc[:, [
+#         'movie_id', 'movie_title', 'release_date', 'poster_url', 'like']]
+#     return json.loads(results.to_json(orient="records"))
 
     
 
--- recommender.py
+++ recommender.py
@@ -1,13 +1,133 @@
-from surprise import SVD
-from surprise import Dataset
-from surprise.model_selection import cross_validate
+import csv
+import os
+import numpy as np
+import pandas as pd
+from scipy import spatial
+from surprise import KNNWithMeans, SVD, Reader, Dataset, dump
 
-# Load the movielens-100k dataset (download it if needed).
-data = Dataset.load_builtin('ml-100k')
-print(data)
-#
-# # Use the famous SVD algorithm.
-# algo = SVD()
-#
-# # Run 5-fold cross-validation and print results.
-# cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
+def get_recommend_items_by_svd(new_user_ratings, n=12):
+    rec_iid_list = []
+    trainset = user_add(new_user_ratings)
+    uid = trainset.n_users
+    item_rid_list = [trainset.to_raw_iid(inner_id) for inner_id in trainset.ir.keys()]
+    print(f'number of total users: {trainset.n_users}')
+    algo = SVD(n_factors=20, n_epochs=30, biased=False)     # optimal parameters from Jupyter Notebook
+    algo.fit(trainset)
+    dump.dump('./model', algo=algo, verbose=1)
+    all_results = {}
+    for i in item_rid_list:
+        uid = str(uid)
+        iid = str(i)
+        pred = algo.predict(uid, iid).est
+        all_results[iid] = pred
+    sorted_list = sorted(all_results.items(),
+                         key=lambda kv: (kv[1], kv[0]), reverse=True)
+    for i in range(n):
+        print(sorted_list[i])
+        rec_iid_list.append(sorted_list[i][0])
+    return uid, rec_iid_list
+
+
+def get_similar_items_by_svd(iid, n=12):
+    svd_algo = dump.load('./model')[1]
+    trainset = svd_algo.trainset
+    # print(f'iis: {iid}, number of total items: {algo.trainset.n_items}')
+    # Option 1: calculating k nearest neighbors by KNNWithMeans algorithm, we use it here.
+    knn_algo = KNNWithMeans(k=60, sim_options={     # optimal parameters from Jupyter Notebook
+        'name': 'cosine',
+        'user_based': False
+    })
+    knn_algo.fit(trainset)
+    inner_id = knn_algo.trainset.to_inner_iid(iid)
+    neighbors = knn_algo.get_neighbors(inner_id, k=n)
+    neighbors_iid_list = [knn_algo.trainset.to_raw_iid(x) for x in neighbors]
+    # Option 2: calculating k nearest neighbors by k_neighbors_item_based function, we don't use it because it executes too slowly.
+    # neighbors_iid_list = k_neighbors_item_based(knn_algo.trainset, iid, n)
+    print("neighbors_iid", neighbors_iid_list)
+    return neighbors_iid_list
+
+
+def user_add(new_user_ratings, is_reset=False):
+    # simulate adding a new user into the original data file
+    if is_reset or not os.path.exists('new_ratings.csv'):
+        df = pd.read_csv('./ratings.csv')
+        df.to_csv('new_' + 'ratings.csv', index=False)
+    new_df = pd.read_csv('./new_ratings.csv', sep='\t', header=None, names=['user', 'item', 'rating', 'timestamp'])
+    uid = int(new_df['user'].max()) + 1
+    with open(r'new_ratings.csv', mode='a', newline='', encoding='utf8') as cfa:
+        wf = csv.writer(cfa, delimiter='\t')
+        for (i, s) in new_user_ratings:
+            if s > 0:
+                new_rating = [str(uid), str(i), int(s), '0']
+                wf.writerow(new_rating)
+    file_path = os.path.expanduser('new_ratings.csv')
+    reader = Reader(line_format='user item rating timestamp', sep='\t')
+    data = Dataset.load_from_file(file_path, reader=reader)
+    trainset = data.build_full_trainset()
+    return trainset
+
+
+# Calculating k nearest neighbors step by step 
+def k_neighbors_item_based(train_set, raw_iid, k):
+
+    train_user_list = [u_id for u_id in train_set.ur.keys()]
+    train_item_list = [i_id for i_id in train_set.ir.keys()]
+
+    # initiate the matrix for storing the similarity values
+    sim_item = np.empty((len(train_item_list), len(train_item_list)))
+
+    # need to calculate the mean rating of each user for adjusted cosine similarity
+    u_rating_mean_dict = {}
+    for u_id in range(len(train_user_list)):
+        sum_r = 0
+        count = 0
+        for (i, r) in train_set.ur[u_id]:
+            sum_r += r
+            count += 1
+        u_rating_mean_dict[u_id] = sum_r / count
+
+    # loop through all item to calculate the similaity
+    for item1_id in range(len(train_item_list)):
+        for item2_id in range(item1_id + 1, len(train_item_list)):
+            # A: find common user that rated both i1 and i2
+            item1_user_rating_dict = {}
+            for (u, r) in train_set.ir[item1_id]:
+                item1_user_rating_dict[u] = r
+            item2_user_rating_dict = {}
+            for (u, r) in train_set.ir[item2_id]:
+                item2_user_rating_dict[u] = r
+            common_users = list(set.intersection(set(item1_user_rating_dict.keys()),
+                                                 set(item2_user_rating_dict.keys())))
+            # B: construct rating vectors of common user for two items
+            item1_rating_list, item2_rating_list = [], []
+            for u in common_users:
+                item1_rating_list.append(
+                    item1_user_rating_dict[u] - u_rating_mean_dict[u])
+                item2_rating_list.append(
+                    item2_user_rating_dict[u] - u_rating_mean_dict[u])
+            # C: calculate the similarities between two items, we use adjusted cosine similarity
+            if len(common_users) > 0:
+                sim = 1 - \
+                    spatial.distance.cosine(
+                        item1_rating_list, item2_rating_list)
+            else:
+                sim = 0
+            # D: store measured similarity into matrix
+            sim_item[item1_id][item2_id] = sim
+            sim_item[item2_id][item1_id] = sim
+
+    # calculating k nearset neighbors
+    i_inner_id = train_set.to_inner_iid(raw_iid)
+
+    neighbors_sim_dict = {}
+    i_sim_item = sim_item[i_inner_id]
+    for i in range(len(i_sim_item)):
+        if i_inner_id != i:
+            neighbors_sim_dict[i] = i_sim_item[i]
+
+    k_neighbors = sorted(neighbors_sim_dict.items(),
+                         reverse=True, key=lambda item: item[1])[:k]
+    k_neighbors_raw_iid = [train_set.to_raw_iid(i) for i, _ in k_neighbors]
+
+    return k_neighbors_raw_iid
